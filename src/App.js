043b0d7ab137201008d88badf137dd87e0023e6b
ldelbel <lucdelbel@gmail.com>
2021-07-05 11:59:00 -0300

    Transfer reset.css to styled-components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
-import './App.css';
 import Footer from './components/Footer';
+import { GlobalStyle } from './components/GlobalStyle';
 import Header from './components/Header';
 import Message from './components/Message';
 import Spinner from './components/Spinner';
@@ -11,6 +11,7 @@ import MessageContext from './contexts/MessageContext';
 import useLoading from './hooks/useLoading';
 import ProductsPage from './pages/products/ProductsPage';
 import CategoriesService from './services/CategoriesService';
+
 
 function App() {
   const [filter, setFilter] = useState('');
@@ -30,21 +31,24 @@ function App() {
   }
 
   return (
-    <FilterContext.Provider value={{ filter, setFilter }}>
-      <LoadingContext.Provider value={{ addRequest, removeRequest, isLoading }}>
-        <MessageContext.Provider value={{ message, setMessage }}>
-          <CategoriesContext.Provider value={{ categories }}>
-            <Spinner></Spinner>
-            <div className="page-container">
-              <Message></Message>
-              <Header></Header>
-              <ProductsPage></ProductsPage>
-            </div>
-            <Footer></Footer>
-          </CategoriesContext.Provider>
-        </MessageContext.Provider>
-      </LoadingContext.Provider>
-    </FilterContext.Provider>
+    <>
+      <GlobalStyle />
+      <FilterContext.Provider value={{ filter, setFilter }}>
+        <LoadingContext.Provider value={{ addRequest, removeRequest, isLoading }}>
+          <MessageContext.Provider value={{ message, setMessage }}>
+            <CategoriesContext.Provider value={{ categories }}>
+              <Spinner></Spinner>
+              <div className="page-container">
+                <Message></Message>
+                <Header></Header>
+                <ProductsPage></ProductsPage>
+              </div>
+              <Footer></Footer>
+            </CategoriesContext.Provider>
+          </MessageContext.Provider>
+        </LoadingContext.Provider>
+      </FilterContext.Provider>
+    </>
   );
 }
 
